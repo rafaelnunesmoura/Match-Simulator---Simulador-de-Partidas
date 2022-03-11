cc44dd74b5bf6e7c6e8bcde2577d8a62cb88298f
rafael <rafaelnunesmoura@protonmail.com>
2022-03-11 10:31:18 -0300

    finalização do layout

---
+++ app/src/main/java/com/example/match_simulator/DetailActivity.kt
@@ -0,0 +1,17 @@
+package com.example.match_simulator
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.match_simulator.databinding.ActivityDetailBinding
+
+class DetailActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityDetailBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityDetailBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+    }
+}
